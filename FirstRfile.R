02793d38623fd82d770c9433187642d15d68e467
Alexander D. Kay <ADKay@ohioauditor.gov>
2025-01-22 15:33:07 -0500

    edit

--- FirstRfile.R
+++ FirstRfile.R
@@ -1,14 +1,22 @@
 library(tidyverse)
 
-ggplot(data = mtcars,
+plotA <- ggplot(data = mtcars,
        aes(x=mpg,
            y=hp,
            size=wt))+
   geom_point()+
   theme_bw()+
+  ggtitle("MPG as HP Increases")+
   coord_flip()
+
+ggsave(filename = "images/MPG HP and WT Plot.png",plot = plotA)
 
 
 linMod1 <- lm(data = mtcars, mpg~hp+wt)
 
 summary(linMod1)
+
+library(stargazer)
+
+stargazer(linMod1,out = "images/linmod1.html")
+
